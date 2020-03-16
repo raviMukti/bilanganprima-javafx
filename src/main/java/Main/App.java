60249e50bf6943cc3db636ef205bf203db009731
raviMukti <shinjiravi@gmail.com>
2020-03-16 17:36:30 +0700

    Initialize

---
+++ src/main/java/Main/App.java
@@ -0,0 +1,25 @@
+package Main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/Dashboard.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Bilangan Prima - Sofielest");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/main/java/controller/DashboardController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class DashboardController implements Initializable {
+
+    @FXML
+    private AnchorPane paneAnchor;
+
+    @FXML
+    private Label lblHelloWorld;
+
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
